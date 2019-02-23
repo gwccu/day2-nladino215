754c2c325ed26b8263ed9245e693484350699833
Maya Lavi <nladino215@gmail.com>
2019-02-23 17:31:49 +0000

    worked on problem

---
+++ day3problem.py
@@ -0,0 +1,23 @@
+number=input("input an integer")
+oddnum=int(0)
+if(int(number) % 2 == 0):
+    print("even")
+elif not(int(number) % 2 == 0):
+    print("odd")
+    oddnum += 1
+number=input("input an integer")
+if(int(number) % 2 == 0):
+    print("even")
+elif not(int(number) % 2 == 0):
+    print("odd")
+    oddnum += 1
+number=input("input an integer")
+if(int(number) % 2 == 0):
+    print("even")
+elif not(int(number) % 2 == 0):
+    print("odd")
+    oddnum += 1
+print(oddnum)
+
+print("find out your weirdness")
+q1
